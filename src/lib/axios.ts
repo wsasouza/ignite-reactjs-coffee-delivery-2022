af263dc420053bb0269ab76ed4c48eadaf51681e
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-09-16 23:57:41 -0300

    Fix: Fix DB

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'https://my-json-server.typicode.com/wsasouza/demo',
-  baseURL: 'http://localhost:3001',
+  baseURL:
+    'https://my-json-server.typicode.com/wsasouza/ignite-reactjs-coffee-delivery-2022',
+  // baseURL: 'http://localhost:3001',
 })
